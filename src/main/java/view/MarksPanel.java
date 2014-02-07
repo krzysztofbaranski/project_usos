f198cd6e9aea39c5121635a551d6f94017df6814
krzysiek <krzysztof.kleiner@gmail.com>
2014-02-07 19:58:06 +0100

    nowy

--- src/main/java/view/MarksPanel.java
+++ src/main/java/view/MarksPanel.java
@@ -20,49 +20,98 @@ public class MarksPanel {
     private JTable table1;
     private JPanel root;
     private JScrollPane scroll1;
+    private JTable table2;
     Vector<Vector<Object>> v;
+    Vector<Vector<Object>> vv;
 
     public MarksPanel() {
-        v = Utility.getData(Learning.getMarks(-1, User.person_id, false));
+        if(User.staffCode == null) {
+            v = Utility.getData(Learning.getMarks(-1, User.person_id, false));
 
-        TableModel dataModel = new AbstractTableModel() {
-            private final Object[] columnNames = {"prowadzący", "grupa",
-                    "ocena", "opis", "uwagi", "data"};
+
+            TableModel dataModel = new AbstractTableModel() {
+                private final Object[] columnNames = {"prowadzący", "grupa",
+                        "ocena", "opis", "uwagi", "data"};
+
+                public String getColumnName(int column) {
+                    return columnNames[column].toString();
+                }
+
+                public int getColumnCount() {
+                    return 6;
+                }
+
+                public int getRowCount() {
+                    return v.size();
+                }
+
+                public Object getValueAt(int row, int col) {
+                    if (col == 0) return v.get(row).get(1) + " " + v.get(row).get(2);
+                    return v.get(row).get(col + 3);
+                }
+            };
+
+
+            table1.setModel(dataModel);
+            table1.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    int row = table1.rowAtPoint(new Point(e.getX(), e.getY()));
+                    int col = table1.columnAtPoint(new Point(e.getX(), e.getY()));
+                    if (row == -1) return;
+                    if (col == 0) {
+                        Vector<Vector<Object>> w = Utility.getData("select mail from persons where id = " + v.get(row).get(0));
+                        app.Window.mainFrame.setContentPane(new AccountPanel((String)w.firstElement().firstElement()).getRoot());
+                        app.Window.mainFrame.setVisible(true);
+
+                    }
+                    if (col == 1) {
+                        System.out.println(v.get(row).get(3));
+                        app.Window.mainFrame.setContentPane(new GroupDetails((long) v.get(row).get(3)).getRoot());
+                        app.Window.mainFrame.setVisible(true);
+                    }
+
+
+                }
+
+            });
+        }
+
+        vv = Utility.getData("select groups.id, groups.name from groups join staff_groups on staff_groups.group_id = groups.id join students_branches__groups on " +
+                "students_branches__groups.group_id = groups.id join students_branches on student_branch_id = students_branches.id" +
+                " where student_id = " + User.person_id + " or staff_id = " + User.person_id);
+
+
+        TableModel dataModel1 = new AbstractTableModel() {
+            private final Object[] columnNames = {"nazwa"};
 
             public String getColumnName(int column) {
                 return columnNames[column].toString();
             }
 
             public int getColumnCount() {
-                return 6;
+                return 1;
             }
 
             public int getRowCount() {
-                return v.size();
+                return vv.size();
             }
 
             public Object getValueAt(int row, int col) {
-                if (col == 0) return v.get(row).get(1) + " " + v.get(row).get(2);
-                return v.get(row).get(col + 3);
+                return vv.get(row).get(1);
             }
         };
 
 
-        table1.setModel(dataModel);
-        table1.addMouseListener(new MouseAdapter() {
+        table2.setModel(dataModel1);
+        table2.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                int row = table1.rowAtPoint(new Point(e.getX(), e.getY()));
-                int col = table1.columnAtPoint(new Point(e.getX(), e.getY()));
+                int row = table2.rowAtPoint(new Point(e.getX(), e.getY()));
+                int col = table2.columnAtPoint(new Point(e.getX(), e.getY()));
                 if (row == -1) return;
                 if (col == 0) {
-                    app.Window.mainFrame.setContentPane(new TeacherDetails((long) v.get(row).get(0)).getRoot());
-                    app.Window.mainFrame.setVisible(true);
-                    //TODO
-                }
-                if (col == 1) {
-                    System.out.println(v.get(row).get(3));
-                    app.Window.mainFrame.setContentPane(new GroupDetails((long) v.get(row).get(3)).getRoot());
+                    app.Window.mainFrame.setContentPane(new GroupDetails((long) vv.get(row).get(0)).getRoot());
                     app.Window.mainFrame.setVisible(true);
                 }
 
