e8f4fcd73b25afdec596d327f08aea9489759ad1
Krzysztof Baranski <baranski.kk@gmail.com>
2014-02-07 11:05:31 +0100

    Usuwanie ostrzezen

--- src/main/java/app/Settings.java
+++ src/main/java/app/Settings.java
@@ -4,9 +4,9 @@ package app;
  * Created by krzysztof on 04/02/14.
  */
 public class Settings {
-    public static final String dbUrl = "//localhost/usos";
-    public static final String username = "postgres";
-    public static final String passwd = "whiskas";
+    public static String dbUrl = "//localhost/usos";
+    public static String username = "krzysztof";
+    public static String passwd = "";
 
     public static final Long superuser = 1L;
 }
